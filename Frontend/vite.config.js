8b3b91dfbb0efd5464f8e461c9af170a27862aa6
Saurabh pal <100469198+Saurabhpal052@users.noreply.github.com>
2024-12-27 00:07:30 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port:3001,
     proxy: {
       "/api": {
-        target: "http://localhost:4002/",
+        target: "https://chatapp-api-three.vercel.app/",
         changeOrigin: true,
         strictPort:true,
       },
